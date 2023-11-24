64c9f67ae401ba04055bea529b824257b57b28c8
McFishhh <slimey536@gmail.com>
2023-11-24 12:48:43 +0800

    fixed forked version

--- main.py
+++ main.py
@@ -13,29 +13,9 @@ def main():
 
     running = True
     while running: 
-        dt = clock.get_time() / 1000
-        
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_p:
-                    if play == True:
-                        play = False
-                    else:
-                        play = True
-                if event.key == pygame.K_RIGHT and play == False:
-                    blockManager.Update(dt)       
-        
-        if play:
-            blockManager.Update(dt)
-            print(dt)
-         
-        screen.fill(Global.BLACK)
-        blockManager.Draw(screen)
-        pygame.display.update()
-        
-        clock.tick(tickRate)
+        running = menu.Detector()
+        menu.Update()
+        menu.Draw(screen)
 
     pygame.quit()
 
